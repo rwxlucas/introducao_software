e2c741e122f71be65376affebbaa8d2953fcb498
Lucas Faria Julio Barroso <lucasfjb.dev@gmail.com>
2025-08-26 17:16:57 -0300

    Finalizado super trunfo lógica do jogo

---
+++ README.md
@@ -0,0 +1,57 @@
+Super Trunfo
+Este é um programa em C chamado Super Trunfo, que permite cadastrar duas cartas com informações de países (código, nome, população, área, PIB e número de pontos turísticos) e comparar um atributo escolhido entre elas para determinar a carta vencedora.
+Requisitos
+
+Sistema Operacional: O programa foi desenvolvido e testado em ambiente Linux.
+Compilador: GCC (GNU Compiler Collection) instalado.
+Bibliotecas: Biblioteca padrão de C (nenhuma biblioteca adicional necessária).
+
+Como Compilar
+O programa está contido no arquivo super_trunfo.c. Para compilá-lo no Linux usando o GCC, siga os passos abaixo:
+
+Abra um terminal no diretório onde o arquivo super_trunfo.c está localizado.
+
+Execute o seguinte comando para compilar o código:
+gcc super_trunfo.c -o super_trunfo
+
+gcc: Invoca o compilador GCC.
+super_trunfo.c: O arquivo fonte do programa.
+-o super_trunfo: Especifica o nome do arquivo executável gerado (super_trunfo).
+
+Isso criará um arquivo executável chamado super_trunfo no mesmo diretório.
+
+Como Executar
+Após compilar o programa, você pode executá-lo com o comando:
+./super_trunfo
+
+Passos de Execução
+
+O programa solicitará o cadastro da primeira carta:
+Código da carta (3 caracteres).
+Nome do país.
+População (em número inteiro).
+Área (em km², número decimal).
+PIB (número decimal).
+Número de pontos turísticos (número inteiro).
+
+O mesmo processo será repetido para a segunda carta.
+Em seguida, você escolherá um atributo para comparação (1 para população, 2 para área, 3 para PIB, 4 para pontos turísticos).
+O programa exibirá o resultado da comparação, indicando a carta vencedora ou um empate.
+
+Notas
+
+Entrada de Dados: Certifique-se de inserir os dados no formato correto (por exemplo, números inteiros para população e pontos turísticos, números decimais para área e PIB). Nomes de países podem incluir espaços.
+Ambiente Linux: O programa foi testado em Linux. Para outros sistemas operacionais, o comando de compilação pode variar (por exemplo, em Windows, você pode usar MinGW ou outro compilador compatível).
+Erros de Compilação: Se encontrar erros, verifique se o GCC está instalado (gcc --version) e se o arquivo super_trunfo.c está no diretório correto.
+
+Exemplo de Uso
+$ gcc super_trunfo.c -o super_trunfo
+$ ./super_trunfo
+Cadastro da primeira carta:
+Código da carta (3 caracteres): BR1
+País: Brasil
+População: 213000000
+Área (em km²): 8515767
+PIB: 1800000000000
+Número de pontos turísticos: 50
+...
--- super_trunfo.c
+++ super_trunfo.c
@@ -2,127 +2,235 @@
 #include <string.h>
 
 int main() {
-  char state1[3];
-  char cardCode1[4];
-  char city1[32];
-  unsigned long int population1;
-  int area1;
-  int pib1;
+  char country1[32];
+  int population1;
+  float area1;
+  float pib1;
   int touristPoints1;
   float popDensity1;
-  float pibPercapta1;
-  float superPower1;
+  char card1[4];
 
-  printf("Cadastro da primeira carta: \n");
+  printf("Cadastro da primeira carta:\n");
 
-  printf("Estado: ");
-  scanf("%s", state1);
-  getchar();
+  printf("Código da carta (3 caracteres): ");
+  scanf("%3s", card1);
+  scanf("%*c");
 
-  printf("Código da carta: ");
-  scanf("%s", cardCode1);
-  getchar();
-
-  printf("Cidade: ");
-  fgets(city1, 32, stdin);
-  city1[strcspn(city1, "\n")] = '\0';
+  printf("País: ");
+  fgets(country1, sizeof(country1), stdin);
+  country1[strcspn(country1, "\n")] = '\0';
 
   printf("População: ");
-  scanf("%lu", &population1);
-  getchar();
+  scanf("%d", &population1);
+  scanf("%*c");
 
   printf("Área (em km²): ");
-  scanf("%d", &area1);
-  getchar();
+  scanf("%f", &area1);
+  scanf("%*c");
 
   printf("PIB: ");
-  scanf("%d", &pib1);
-  getchar();
+  scanf("%f", &pib1);
+  scanf("%*c");
 
   printf("Número de pontos turísticos: ");
   scanf("%d", &touristPoints1);
+  scanf("%*c");
 
   popDensity1 = (float)population1 / area1;
-  pibPercapta1 = (float)pib1 / population1;
-  superPower1 = (float)population1 + (float)area1 + (float)pib1 +
-                (float)touristPoints1 + pibPercapta1 + (1 / popDensity1);
 
-  char state2[3];
-  char cardCode2[4];
-  char city2[32];
-  unsigned long int population2;
-  int area2;
-  int pib2;
+  char country2[32];
+  int population2;
+  float area2;
+  float pib2;
   int touristPoints2;
   float popDensity2;
-  float pibPercapta2;
-  float superPower2;
+  char card2[4];
 
-  printf("\nCadastro da segunda carta: \n");
+  printf("\nCadastro da segunda carta:\n");
 
-  printf("Estado: ");
-  scanf("%s", state2);
-  getchar();
+  printf("Código da carta (3 caracteres): ");
+  scanf("%3s", card2);
+  scanf("%*c");
 
-  printf("Código da carta: ");
-  scanf("%s", cardCode2);
-  getchar();
-
-  printf("Cidade: ");
-  fgets(city2, 32, stdin);
-  city2[strcspn(city2, "\n")] = '\0';
+  printf("País: ");
+  fgets(country2, sizeof(country2), stdin);
+  country2[strcspn(country2, "\n")] = '\0';
 
   printf("População: ");
-  scanf("%lu", &population2);
-  getchar();
+  scanf("%d", &population2);
+  scanf("%*c");
 
   printf("Área (em km²): ");
-  scanf("%d", &area2);
-  getchar();
+  scanf("%f", &area2);
+  scanf("%*c");
 
   printf("PIB: ");
-  scanf("%d", &pib2);
-  getchar();
+  scanf("%f", &pib2);
+  scanf("%*c");
 
   printf("Número de pontos turísticos: ");
   scanf("%d", &touristPoints2);
+  scanf("%*c");
 
   popDensity2 = (float)population2 / area2;
-  pibPercapta2 = (float)pib2 / population2;
 
-  superPower2 = (float)population2 + (float)area2 + (float)pib2 +
-                (float)touristPoints2 + pibPercapta2 + (2 / popDensity2);
+  printf("\nComparação de Cartas:\n");
 
-  printf("\n");
+  int firstAttribute, secondAttribute;
+  printf("Escolha o primeiro atributo:\n");
+  printf("1. População\n");
+  printf("2. Área\n");
+  printf("3. PIB\n");
+  printf("4. Número de pontos turísticos\n");
+  printf("5. Densidade demográfica\n");
+  scanf("%d", &firstAttribute);
+  scanf("%*c");
 
-  printf("Comparação de Cartas:\n");
+  printf("\nEscolha o segundo atributo (diferente do primeiro):\n");
+  if (firstAttribute != 1) printf("1. População\n");
+  if (firstAttribute != 2) printf("2. Área\n");
+  if (firstAttribute != 3) printf("3. PIB\n");
+  if (firstAttribute != 4) printf("4. Número de pontos turísticos\n");
+  if (firstAttribute != 5) printf("5. Densidade demográfica\n");
+  do {
+    scanf("%d", &secondAttribute);
+    scanf("%*c");
+    if (secondAttribute == firstAttribute) {
+      printf(
+          "Erro: O segundo atributo deve ser diferente do primeiro. Tente "
+          "novamente:\n");
+    }
+  } while (secondAttribute == firstAttribute || secondAttribute < 1 ||
+           secondAttribute > 5);
 
-  int cardPop1Win = population1 > population2 ? 1 : 2;
-  printf("População: Carta %d venceu (%s)\n", cardPop1Win,
-         cardPop1Win == 1 ? "1" : "0");
+  char attribute1[30], attribute2[30];
+  float value1_1 = 0, value1_2 = 0, value2_1 = 0, value2_2 = 0;
+  int winner1 = 0, winner2 = 0;
 
-  int cardArea1Win = area1 > area2 ? 1 : 2;
-  printf("Área: Carta %d venceu (%s)\n", cardArea1Win,
-         cardArea1Win == 1 ? "1" : "0");
+  switch (firstAttribute) {
+    case 1:
+      strcpy(attribute1, "População");
+      value1_1 = population1;
+      value2_1 = population2;
+      if (population1 > population2)
+        winner1++;
+      else if (population2 > population1)
+        winner2++;
+      break;
+    case 2:
+      strcpy(attribute1, "Área");
+      value1_1 = area1;
+      value2_1 = area2;
+      if (area1 > area2)
+        winner1++;
+      else if (area2 > area1)
+        winner2++;
+      break;
+    case 3:
+      strcpy(attribute1, "PIB");
+      value1_1 = pib1;
+      value2_1 = pib2;
+      if (pib1 > pib2)
+        winner1++;
+      else if (pib2 > pib1)
+        winner2++;
+      break;
+    case 4:
+      strcpy(attribute1, "Número de pontos turísticos");
+      value1_1 = touristPoints1;
+      value2_1 = touristPoints2;
+      if (touristPoints1 > touristPoints2)
+        winner1++;
+      else if (touristPoints2 > touristPoints1)
+        winner2++;
+      break;
+    case 5:
+      strcpy(attribute1, "Densidade demográfica");
+      value1_1 = popDensity1;
+      value2_1 = popDensity2;
+      if (popDensity1 < popDensity2)
+        winner1++;
+      else if (popDensity2 < popDensity1)
+        winner2++;
+      break;
+    default:
+      printf("Opção inválida\n");
+      return 1;
+  }
 
-  int cardPib1Win = pib1 > pib2 ? 1 : 2;
-  printf("PIB: Carta %d venceu (%s)\n", cardPib1Win, cardPib1Win == 1 ? "1" : "0");
+  switch (secondAttribute) {
+    case 1:
+      strcpy(attribute2, "População");
+      value1_2 = population1;
+      value2_2 = population2;
+      if (population1 > population2)
+        winner1++;
+      else if (population2 > population1)
+        winner2++;
+      break;
+    case 2:
+      strcpy(attribute2, "Área");
+      value1_2 = area1;
+      value2_2 = area2;
+      if (area1 > area2)
+        winner1++;
+      else if (area2 > area1)
+        winner2++;
+      break;
+    case 3:
+      strcpy(attribute2, "PIB");
+      value1_2 = pib1;
+      value2_2 = pib2;
+      if (pib1 > pib2)
+        winner1++;
+      else if (pib2 > pib1)
+        winner2++;
+      break;
+    case 4:
+      strcpy(attribute2, "Número de pontos turísticos");
+      value1_2 = touristPoints1;
+      value2_2 = touristPoints2;
+      if (touristPoints1 > touristPoints2)
+        winner1++;
+      else if (touristPoints2 > touristPoints1)
+        winner2++;
+      break;
+    case 5:
+      strcpy(attribute2, "Densidade demográfica");
+      value1_2 = popDensity1;
+      value2_2 = popDensity2;
+      if (popDensity1 < popDensity2)
+        winner1++;
+      else if (popDensity2 < popDensity1)
+        winner2++;
+      break;
+    default:
+      printf("Opção inválida\n");
+      return 1;
+  }
 
-  int cardTuristic1Win = touristPoints1 > touristPoints2 ? 1 : 2;
-  printf("Pontos Turísticos: Carta %d venceu (%s)\n", cardTuristic1Win,
-         cardTuristic1Win == 1 ? "1" : "0");
+  float sum1 = value1_1 + value1_2;
+  float sum2 = value2_1 + value2_2;
 
-  int cardPopulationDen1Win = popDensity1 < popDensity2 ? 1 : 2;
-  printf("Densidade Populacional: Carta %d venceu (%s)\n",
-         cardPopulationDen1Win, cardPopulationDen1Win == 1 ? "1" : "0");
+  printf("\n=== Resultado da Comparação ===\n");
+  printf("Países: %s (%s) vs %s (%s)\n", country1, card1, country2, card2);
+  printf("Atributo 1: %s\n", attribute1);
+  printf("  %s: %.2f\n", country1, value1_1);
+  printf("  %s: %.2f\n", country2, value2_1);
+  printf("Atributo 2: %s\n", attribute2);
+  printf("  %s: %.2f\n", country1, value1_2);
+  printf("  %s: %.2f\n", country2, value2_2);
+  printf("Soma dos atributos:\n");
+  printf("  %s: %.2f\n", country1, sum1);
+  printf("  %s: %.2f\n", country2, sum2);
 
-  int cardPibPerCapta1Win = pibPercapta1 > pibPercapta2 ? 1 : 2;
-  printf("PIB per Capita: Carta %d venceu (%s)\n", cardPibPerCapta1Win,
-         cardPibPerCapta1Win == 1 ? "1" : "0");
-
-  int cardSuperpower1Win = superPower1 > superPower2 ? 1 : 2;
-  printf("Super Poder: Carta %d venceu (%s)\n", cardSuperpower1Win,
-         cardSuperpower1Win == 1 ? "1" : "0");
+  if (sum1 > sum2) {
+    printf("Carta vencedora: %s (%s)\n", card1, country1);
+  } else if (sum2 > sum1) {
+    printf("Carta vencedora: %s (%s)\n", card2, country2);
+  } else {
+    printf("Empate!\n");
+  }
 
   return 0;
 }

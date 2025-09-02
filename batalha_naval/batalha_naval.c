2975c5638db7cd4570e226e9848a613b8df4c0cc
Lucas Faria Julio Barroso <lucasfjb.dev@gmail.com>
2025-09-02 16:12:42 -0300

    navios na diagonal

--- batalha_naval/batalha_naval.c
+++ batalha_naval/batalha_naval.c
@@ -7,17 +7,35 @@
 void inicializarTabuleiro(int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO]) {
   for (int i = 0; i < TAM_TABULEIRO; i++) {
     for (int j = 0; j < TAM_TABULEIRO; j++) {
-      tabuleiro[i][j] = 0; // Água
+      tabuleiro[i][j] = 0;
     }
   }
 }
 
 bool validarPosicaoNavio(int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO], int linha,
-                         int coluna, int tamanho, bool horizontal) {
-  if (horizontal) {
+                         int coluna, int tamanho, bool horizontal,
+                         bool diagonal, bool diagonal_decrescente) {
+  if (diagonal) {
+    if (diagonal_decrescente) {
+
+      if (linha + tamanho > TAM_TABULEIRO || coluna - tamanho + 1 < 0)
+        return false;
+      for (int k = 0; k < tamanho; k++) {
+        if (tabuleiro[linha + k][coluna - k] != 0)
+          return false;
+      }
+    } else {
+
+      if (linha + tamanho > TAM_TABULEIRO || coluna + tamanho > TAM_TABULEIRO)
+        return false;
+      for (int k = 0; k < tamanho; k++) {
+        if (tabuleiro[linha + k][coluna + k] != 0)
+          return false;
+      }
+    }
+  } else if (horizontal) {
     if (coluna + tamanho > TAM_TABULEIRO)
       return false;
-
     for (int j = coluna; j < coluna + tamanho; j++) {
       if (tabuleiro[linha][j] != 0)
         return false;
@@ -25,7 +43,6 @@ bool validarPosicaoNavio(int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO], int linha,
   } else {
     if (linha + tamanho > TAM_TABULEIRO)
       return false;
-
     for (int i = linha; i < linha + tamanho; i++) {
       if (tabuleiro[i][coluna] != 0)
         return false;
@@ -35,14 +52,26 @@ bool validarPosicaoNavio(int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO], int linha,
 }
 
 bool posicionarNavio(int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO], int linha,
-                     int coluna, int tamanho, bool horizontal) {
-
-  if (!validarPosicaoNavio(tabuleiro, linha, coluna, tamanho, horizontal)) {
+                     int coluna, int tamanho, bool horizontal, bool diagonal,
+                     bool diagonal_decrescente) {
+  if (!validarPosicaoNavio(tabuleiro, linha, coluna, tamanho, horizontal,
+                           diagonal, diagonal_decrescente)) {
     printf("Erro: Posição inválida para o navio em (%d, %d)\n", linha, coluna);
     return false;
   }
 
-  if (horizontal) {
+  if (diagonal) {
+    if (diagonal_decrescente) {
+
+      for (int k = 0; k < tamanho; k++) {
+        tabuleiro[linha + k][coluna - k] = 3;
+      }
+    } else {
+      for (int k = 0; k < tamanho; k++) {
+        tabuleiro[linha + k][coluna + k] = 3;
+      }
+    }
+  } else if (horizontal) {
     for (int j = coluna; j < coluna + tamanho; j++) {
       tabuleiro[linha][j] = 3;
     }
@@ -77,17 +106,28 @@ int main() {
 
   int navio1_linha = 2, navio1_coluna = 3;
   int navio2_linha = 5, navio2_coluna = 7;
+  int navio3_linha = 0, navio3_coluna = 0;
+  int navio4_linha = 0, navio4_coluna = 9;
 
-  if (posicionarNavio(tabuleiro, navio1_linha, navio1_coluna, TAM_NAVIO,
-                      true)) {
+  if (posicionarNavio(tabuleiro, navio1_linha, navio1_coluna, TAM_NAVIO, true,
+                      false, false)) {
     printf("Navio horizontal posicionado em (%d, %d)\n", navio1_linha,
            navio1_coluna);
   }
-
-  if (posicionarNavio(tabuleiro, navio2_linha, navio2_coluna, TAM_NAVIO,
-                      false)) {
+  if (posicionarNavio(tabuleiro, navio2_linha, navio2_coluna, TAM_NAVIO, false,
+                      false, false)) {
     printf("Navio vertical posicionado em (%d, %d)\n", navio2_linha,
            navio2_coluna);
+  }
+  if (posicionarNavio(tabuleiro, navio3_linha, navio3_coluna, TAM_NAVIO, false,
+                      true, false)) {
+    printf("Navio diagonal crescente posicionado em (%d, %d)\n", navio3_linha,
+           navio3_coluna);
+  }
+  if (posicionarNavio(tabuleiro, navio4_linha, navio4_coluna, TAM_NAVIO, false,
+                      true, true)) {
+    printf("Navio diagonal decrescente posicionado em (%d, %d)\n", navio4_linha,
+           navio4_coluna);
   }
 
   printf("\nTabuleiro com navios posicionados:\n");
